c71acf58b12f7d93c0660755b96449830901843b
Natalia D <90103744+Nattgd@users.noreply.github.com>
2022-03-20 16:17:32 -0300

    Add files via upload

---
+++ GIT/suma.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace GIT
+{
+  class Program1
+ {
+ static void Main(string[] args)
+ {
+ int suma=0; int contar = 0;
+ 
+ while (contar < 10)
+ {
+     contar = contar +1;
+     suma = suma + contar;
+ 
+     Console.WriteLine(suma);
+ }
+ 
+ 
+ 
+ } //fin Main
+ } 
+}
